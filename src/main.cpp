6f30493bc7d0de16fb468db693148b3f7a557a7a
bubbik4 <133143554+bubbik4@users.noreply.github.com>
2026-01-30 15:57:57 +0100

    Add left button support with debounce
    Define BTN_LEFT in config.h and wire it as INPUT_PULLUP in setup. Add debounce state (lastBtnState, lastDebounceTime, debounceDelay) and logic in loop to detect stable presses (active LOW). On a confirmed press the code logs a click and calls display.moveSelection(1). Also call display.drawMenu() during setup to show the menu on boot.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,15 @@
 DisplayManager display;
 NetManager net;
 
+bool lastBtnState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Initializing...");
+
+  pinMode(BTN_LEFT, INPUT_PULLUP);
   
   display.begin();
   display.drawBootScreen();
@@ -17,15 +23,29 @@ void setup() {
   net.begin();
 
   LOG("System UP");
+  display.drawMenu();
 }
 
 
 void loop() {
   net.update();
-  // temp main screen
-  // display.drawMenu();
+  int reading  = digitalRead(BTN_LEFT);
 
-  delay(1000);
-  display.moveSelection(1);
+  if(reading != lastBtnState) {
+    lastDebounceTime = millis();
+  } 
 
+  if((millis() - lastDebounceTime) > debounceDelay) {
+
+    static int buttonState = HIGH;
+    if(reading != buttonState) {
+      buttonState = reading;
+
+      if(buttonState == LOW) {
+        LOG("Click!");
+        display.moveSelection(1);
+      }
+    }
+  }
+  lastBtnState = reading;
 }
